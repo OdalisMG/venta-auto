4dc2fa6d13673cac4aeb880b1b8e7a101bb5a930
OdalisMG <godalis64@gmail.com>
2024-04-01 10:16:46 -0500

    Cree utilitario

--- src/app/paginas/PagListaVehiculos/PagListaVehiculos.component.ts
+++ src/app/paginas/PagListaVehiculos/PagListaVehiculos.component.ts
@@ -9,6 +9,9 @@ export class PagListaVehiculosComponent implements OnInit {
 
   mostrarImagen = true;
   filtro:string = "";
+
+  @Input() valor:string = '';
+  
   constructor() { }
 
   ngOnInit() {
@@ -19,9 +22,9 @@ export class PagListaVehiculosComponent implements OnInit {
   }
 
   public listavehiculos: Array<any> = [
-    {"codigo": "A001","marca": "FORD", "modelo":"RANGER","color":"negro","kilometraje": "45000","precio":"12000" ,"foto": null,"anio":2022,"calificacion":5},
-    {"codigo": "A001","marca": "FORD", "modelo":"RANGER","color":"negro","kilometraje": "45000","precio":"12000" ,"foto": null,"anio":2022,"calificacion":5},
-    {"codigo": "A001","marca": "FORD", "modelo":"RANGER","color":"negro","kilometraje": "45000","precio":"12000" ,"foto":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_11eb957fb39b4b45be7558197979a0e9.jpg","anio":2022,"calificacion":5},
+    {"codigo": "A001","marca": "FORD", "modelo":"RANGER-6","color":"negro","kilometraje": "45000","precio":"12000" ,"foto": null,"anio":2022,"calificacion":5},
+    {"codigo": "A001","marca": "FORD", "modelo":"RANGER-2","color":"negro","kilometraje": "45000","precio":"12000" ,"foto": null,"anio":2022,"calificacion":5},
+    {"codigo": "A001","marca": "FORD", "modelo":"RANGER-5","color":"negro","kilometraje": "45000","precio":"12000" ,"foto":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_11eb957fb39b4b45be7558197979a0e9.jpg","anio":2022,"calificacion":5},
     {"codigo": "A001","marca": "FORD", "modelo":"RANGER","color":"negro","kilometraje": "45000","precio":"12000" ,"foto":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_11eb957fb39b4b45be7558197979a0e9.jpg","anio":2022,"calificacion":5},
     {"codigo": "A001","marca": "FORD", "modelo":"RANGER","color":"negro","kilometraje": "45000","precio":"12000" ,"foto":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_11eb957fb39b4b45be7558197979a0e9.jpg","anio":2022,"calificacion":5}
   
--- src/app/paginas/PaginaModule.ts
+++ src/app/paginas/PaginaModule.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { PagListaVehiculosComponent } from './PagListaVehiculos/PagListaVehiculos.component';
 import { CommonModule } from "@angular/common"
 import { FormsModule } from "@angular/forms";
+import { UtilitariosModule }  from "../utilitarios/UtilitariosModule";
 
 @NgModule({
     imports:[
         CommonModule,
-        FormsModule
+        FormsModule,
+        UtilitariosModule
     ],
     declarations:[
         PagListaVehiculosComponent
     ],
     exports: [
         PagListaVehiculosComponent
+
     ]
 
 })
